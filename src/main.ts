63a4993ce49fa4fc2cf07da0aa96c00a67211f38
nebarf <francesco.benedetto@sistinf.it>
2021-05-18 16:30:23 +0200

    Fix compilation error

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,22 @@
 import './polyfills';
 
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, NgModuleRef } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+declare global {
+  interface Window {
+    ngRef: NgModuleRef<AppModule>;
+  }
+}
+
 platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
   // Ensure Angular destroys itself on hot reloads.
-  if (window['ngRef']) {
-    window['ngRef'].destroy();
+  if (window.ngRef) {
+    window.ngRef.destroy();
   }
-  window['ngRef'] = ref;
+  window.ngRef = ref;
 
   // Otherwise, log the boot error
 }).catch(err => console.error(err));
